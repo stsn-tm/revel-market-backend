a40a4a04b3932341f9c54397f7a5f6af1728656a
stsn <b5850235@gmil.com>
2020-02-01 14:05:46 +0700

    updateShopDetail

--- src/GOBALS.js
+++ src/GOBALS.js
@@ -7,8 +7,8 @@
 // }
 // *************HOST************* */
 export default {
-	URL: 'http://13.229.233.71:5001/',
-	URL_IMG: 'http://13.229.233.71:5001/',
-	URL_UPLOAD: 'http://13.229.233.71:5001/upload-image/',
-	URL_DELETE: 'http://13.229.233.71:5001/delete-image/',
+	URL: 'http://191.1.3.63:5001/',
+	URL_IMG: 'http://191.1.3.63:5001/',
+	URL_UPLOAD: 'http://191.1.3.63:5001/upload-image/',
+	URL_DELETE: 'http://191.1.3.63:5001/delete-image/',
 }
